88de1cb572b5d40391bcc0c1f0cad4052ef91f6b
jiahan <jiahantan96@gmail.com>
2018-08-21 20:43:58 -0500

    adding count for levels

--- src/BTMaxLevel.java
+++ src/BTMaxLevel.java
@@ -16,24 +16,45 @@ public class BTMaxLevel {
 		Queue <TreeNode> queue = new LinkedList<>();
 		queue.add( root );
 		
+		int levela = 0;
+		int levelb = 0;
+		
+		int height = 0;
+		
 		while(!queue.isEmpty()){
 			
 			TreeNode current = queue.peek();
 			
-			if( current.left != null){
-				queue.add( current.left );
+			height = queue.size();
+			
+			while(height > 0){
+				
+				System.out.print(current.val + " " );
+			
+				if( current.left != null){
+					queue.add( current.left );
+				}
+				if( current.right != null){
+					queue.add( current.right );				
+				}
+				
+				height--;
 			}
-			if( current.right != null){
-				queue.add( current.right );				
-			}
+			
+			System.out.println();
 			
 			queue.poll();
 			
 		}
 		
 		
+		if(levela > levelb)
+			return levela;
+		else
+			return levelb;
 		
-		return 0;
+		
+//		return 0;
 		
 	}
 
