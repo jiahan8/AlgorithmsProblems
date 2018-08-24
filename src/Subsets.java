87d4543ea40eebb87cc1fc4211dd70520cfe06ce
jiahan <jiahantan96@gmail.com>
2018-08-23 23:42:31 -0500

    begin generate subsets

---
+++ src/Subsets.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+	
+	
+	public List<List<Integer>> subsets(int [] nums){
+		
+		
+		// given a distinct integers array, return all possible subsets
+		
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		
+		List<Integer> subset = new ArrayList<>();
+		
+		subset.add(10);
+		
+		
+		
+		
+		
+		return null;
+		
+	}
+	
+
+}
