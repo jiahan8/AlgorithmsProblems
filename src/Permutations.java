f66a66cee75622f41a0e57225b2a34b17d3cad00
jiahan <jiahantan96@gmail.com>
2018-08-28 23:27:56 -0500

    begins permutations

---
+++ src/Permutations.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+	
+	public List<List<Integer>> permute(int [] nums){
+		
+		List <List<Integer>> result = new ArrayList<>();
+		
+		
+		
+		
+		return result;
+		
+	}
+
+}
